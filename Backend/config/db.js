32e004866f1102937b83ce16118b3b6b3016d540
11elvn <turkibenalimustafa@gmail.com>
2026-05-12 03:12:57 +0200

    Update DB connection to use environment variables

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/saas-ecommerce");
+    // هنا نجعله يقرأ رابط الأطلس من الـ Environment Variables التي وضعناها في Render
+    // وإذا لم يجدها (مثلاً في جهازك) سيستخدم الرابط المحلي
+    const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/saas-ecommerce";
+    
+    await mongoose.connect(mongoURI);
     console.log("MongoDB connected 🚀");
   } catch (error) {
     console.log("DB error:", error);
